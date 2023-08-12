3afbbf7a406bcb14276c0a247cc43b18117a9926
CarlosAlegreUr <CarlosAlegreUr@users.noreply.github.com>
2023-08-12 18:38:34 +0200

    invariants commented out till its completion

--- test/foundry/ICGHandler.t.sol
+++ test/foundry/ICGHandler.t.sol
@@ -1,260 +1,262 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-import {Test} from "../../lib/forge-std/src/Test.sol";
-import "../../lib/forge-std/src/console.sol";
+// TODO: ON DEVELOPMENT
 
-import {IInputControlGlobal} from "contracts/decentralized/globalVersion/IInputControlGlobal.sol";
-import {UseCaseContractGlobal} from "contracts/decentralized/globalVersion/UseCaseContractGlobal.sol";
+// import {Test} from "../../lib/forge-std/src/Test.sol";
+// import "../../lib/forge-std/src/console.sol";
 
-// We supose contracts implemented ICG correctly.
-contract HandlerICG is Test {
-    /**
-     *
-     *  DATA STRUCTURES USED
-     *
-     */
+// import {IInputControlGlobal} from "contracts/decentralized/globalVersion/IInputControlGlobal.sol";
+// import {UseCaseContractGlobal} from "contracts/decentralized/globalVersion/UseCaseContractGlobal.sol";
 
-    struct UseCaseContractInputs {
-        uint256 num;
-        address addr;
-        bytes32 id;
-    }
+// // We supose contracts implemented ICG correctly.
+// contract HandlerICG is Test {
+//     /**
+//      *
+//      *  DATA STRUCTURES USED
+//      *
+//      */
 
-    // Functions used (only 1 so far myFunc)
-    string public myFuncSig = "myFunc(uint256,address)";
-    bytes4 public myFuncSelec = bytes4(keccak256(bytes(myFuncSig)));
+//     struct UseCaseContractInputs {
+//         uint256 num;
+//         address addr;
+//         bytes32 id;
+//     }
 
-    // Events used
-    event InputControlGlobal__InputsPermissionGranted(
-        IInputControlGlobal.Permission indexed permission, IInputControlGlobal.PermissionState state
-    );
+//     // Functions used (only 1 so far myFunc)
+//     string public myFuncSig = "myFunc(uint256,address)";
+//     bytes4 public myFuncSelec = bytes4(keccak256(bytes(myFuncSig)));
 
-    // Shortened states access
-    IInputControlGlobal.PermissionState public unordered = IInputControlGlobal.PermissionState.IS_UNORDERED;
-    IInputControlGlobal.PermissionState public sequence = IInputControlGlobal.PermissionState.IS_SEQUENCE;
-    IInputControlGlobal.PermissionState public notExisting = IInputControlGlobal.PermissionState.IS_NOT_EXISTING;
+//     // Events used
+//     event InputControlGlobal__InputsPermissionGranted(
+//         IInputControlGlobal.Permission indexed permission, IInputControlGlobal.PermissionState state
+//     );
 
-    uint256 public constant NUM_OF_INPUTS_ALLOWED = 10;
-    UseCaseContractInputs[] public inputs;
-    address[] public ownersOfContracts;
-    address[] public contractsImplemetingICG;
-    address public callersUsed;
-    IInputControlGlobal public inputControlGlobal;
+//     // Shortened states access
+//     IInputControlGlobal.PermissionState public unordered = IInputControlGlobal.PermissionState.IS_UNORDERED;
+//     IInputControlGlobal.PermissionState public sequence = IInputControlGlobal.PermissionState.IS_SEQUENCE;
+//     IInputControlGlobal.PermissionState public notExisting = IInputControlGlobal.PermissionState.IS_NOT_EXISTING;
 
-    // A strcut used to keep track user interactions with contracts to later check in invariants
-    // if any of them lead to a bad interaction.
-    struct UserState {
-        address user;
-        address contractAddr;
-        address ownerOfContract;
-        bytes4 funcSelec;
-        IInputControlGlobal.PermissionState lastState;
-    }
+//     uint256 public constant NUM_OF_INPUTS_ALLOWED = 10;
+//     UseCaseContractInputs[] public inputs;
+//     address[] public ownersOfContracts;
+//     address[] public contractsImplemetingICG;
+//     address public callersUsed;
+//     IInputControlGlobal public inputControlGlobal;
 
-    mapping(bytes32 => UserState) public interactionsJudge;
+//     // A strcut used to keep track user interactions with contracts to later check in invariants
+//     // if any of them lead to a bad interaction.
+//     struct UserState {
+//         address user;
+//         address contractAddr;
+//         address ownerOfContract;
+//         bytes4 funcSelec;
+//         IInputControlGlobal.PermissionState lastState;
+//     }
 
-    bool public DETECTED_BAD_STATE_CHANGE = false;
-    bool public DETECTED_USED_CONTRACT_WITHOUT_PERMISSION = false;
-    bool public DETECTED_BAD_STORAGE_CLEAN_UP = false;
+//     mapping(bytes32 => UserState) public interactionsJudge;
 
-    struct ValidInteraction {
-        address contractAddr;
-        uint256 inputsNum;
-        bool isSequence;
-    }
+//     bool public DETECTED_BAD_STATE_CHANGE = false;
+//     bool public DETECTED_USED_CONTRACT_WITHOUT_PERMISSION = false;
+//     bool public DETECTED_BAD_STORAGE_CLEAN_UP = false;
 
-    struct UserProfile {
-        bool hasCalls;
-        uint256 callsLeft;
-        ValidInteraction[] validInteractions;
-        uint256[] invalidInteractionsIndexes;
-    }
+//     struct ValidInteraction {
+//         address contractAddr;
+//         uint256 inputsNum;
+//         bool isSequence;
+//     }
 
-    address[] public usersWithProfiles;
-    mapping(address => UserProfile) public usersProfiles;
+//     struct UserProfile {
+//         bool hasCalls;
+//         uint256 callsLeft;
+//         ValidInteraction[] validInteractions;
+//         uint256[] invalidInteractionsIndexes;
+//     }
 
-    constructor(
-        address[] memory _contractsImplemetingICG,
-        address[] memory _ownersOfContracts,
-        address inputControlAddress
-    ) {
-        for (uint256 i = 0; i < _contractsImplemetingICG.length; i++) {
-            contractsImplemetingICG.push(_contractsImplemetingICG[i]);
-            ownersOfContracts.push(_ownersOfContracts[i]);
-        }
+//     address[] public usersWithProfiles;
+//     mapping(address => UserProfile) public usersProfiles;
 
-        for (uint256 i = 0; i < NUM_OF_INPUTS_ALLOWED; i++) {
-            UseCaseContractInputs memory u = UseCaseContractInputs({
-                num: i,
-                addr: address(uint160(i)),
-                id: keccak256(abi.encode(address(uint160(i)), i))
-            });
-            inputs.push(u);
-        }
+//     constructor(
+//         address[] memory _contractsImplemetingICG,
+//         address[] memory _ownersOfContracts,
+//         address inputControlAddress
+//     ) {
+//         for (uint256 i = 0; i < _contractsImplemetingICG.length; i++) {
+//             contractsImplemetingICG.push(_contractsImplemetingICG[i]);
+//             ownersOfContracts.push(_ownersOfContracts[i]);
+//         }
 
-        inputControlGlobal = IInputControlGlobal(inputControlAddress);
-    }
+//         for (uint256 i = 0; i < NUM_OF_INPUTS_ALLOWED; i++) {
+//             UseCaseContractInputs memory u = UseCaseContractInputs({
+//                 num: i,
+//                 addr: address(uint160(i)),
+//                 id: keccak256(abi.encode(address(uint160(i)), i))
+//             });
+//             inputs.push(u);
+//         }
 
-    /**
-     *
-     *  HELPER FUNCTIONS
-     *
-     */
+//         inputControlGlobal = IInputControlGlobal(inputControlAddress);
+//     }
 
-    function _createPermission(address _allower, address _contractAddress, bytes4 _functionSelector, address _caller)
-        private
-        pure
-        returns (IInputControlGlobal.Permission memory)
-    {
-        IInputControlGlobal.Permission memory p = IInputControlGlobal.Permission({
-            allower: _allower,
-            contractAddress: _contractAddress,
-            functionSelector: _functionSelector,
-            caller: _caller
-        });
-        return p;
-    }
+//     /**
+//      *
+//      *  HELPER FUNCTIONS
+//      *
+//      */
 
-    function _compareArrays(bytes32[] memory arr1, bytes32[] memory arr2) private pure returns (bool areEqual) {
-        if (arr1.length != arr2.length) return false;
-        areEqual = true;
-        for (uint256 i = 0; i < arr1.length; i++) {
-            if (arr1[i] != arr2[i]) {
-                areEqual = false;
-                break;
-            }
-        }
-        return areEqual;
-    }
+//     function _createPermission(address _allower, address _contractAddress, bytes4 _functionSelector, address _caller)
+//         private
+//         pure
+//         returns (IInputControlGlobal.Permission memory)
+//     {
+//         IInputControlGlobal.Permission memory p = IInputControlGlobal.Permission({
+//             allower: _allower,
+//             contractAddress: _contractAddress,
+//             functionSelector: _functionSelector,
+//             caller: _caller
+//         });
+//         return p;
+//     }
 
-    function _getUserStateId(UserState memory _u) private pure returns (bytes32) {
-        return keccak256(abi.encode(_u.user, _u.contractAddr, _u.ownerOfContract, _u.funcSelec, _u.lastState));
-    }
+//     function _compareArrays(bytes32[] memory arr1, bytes32[] memory arr2) private pure returns (bool areEqual) {
+//         if (arr1.length != arr2.length) return false;
+//         areEqual = true;
+//         for (uint256 i = 0; i < arr1.length; i++) {
+//             if (arr1[i] != arr2[i]) {
+//                 areEqual = false;
+//                 break;
+//             }
+//         }
+//         return areEqual;
+//     }
 
-    function _updateUserState(
-        address _user,
-        address _contractAddress,
-        address _contractOwner,
-        bytes4 _funcSelec,
-        IInputControlGlobal.PermissionState _expectedState
-    ) private {
-        // Saving interaction state
-        IInputControlGlobal.Permission memory p =
-            _createPermission(_contractAddress, _contractAddress, myFuncSelec, _user);
-        IInputControlGlobal.PermissionState ps = inputControlGlobal.getPermissionState(p);
-        UserState memory us = UserState({
-            user: _user,
-            contractAddr: _contractAddress,
-            ownerOfContract: _contractOwner,
-            funcSelec: _funcSelec,
-            lastState: ps
-        });
-        bytes32 id = _getUserStateId(us);
-        interactionsJudge[id] = us;
+//     function _getUserStateId(UserState memory _u) private pure returns (bytes32) {
+//         return keccak256(abi.encode(_u.user, _u.contractAddr, _u.ownerOfContract, _u.funcSelec, _u.lastState));
+//     }
 
-        if (_expectedState != ps) {
-            DETECTED_BAD_STATE_CHANGE = true;
-        }
-    }
+//     function _updateUserState(
+//         address _user,
+//         address _contractAddress,
+//         address _contractOwner,
+//         bytes4 _funcSelec,
+//         IInputControlGlobal.PermissionState _expectedState
+//     ) private {
+//         // Saving interaction state
+//         IInputControlGlobal.Permission memory p =
+//             _createPermission(_contractAddress, _contractAddress, myFuncSelec, _user);
+//         IInputControlGlobal.PermissionState ps = inputControlGlobal.getPermissionState(p);
+//         UserState memory us = UserState({
+//             user: _user,
+//             contractAddr: _contractAddress,
+//             ownerOfContract: _contractOwner,
+//             funcSelec: _funcSelec,
+//             lastState: ps
+//         });
+//         bytes32 id = _getUserStateId(us);
+//         interactionsJudge[id] = us;
 
-    function _updateUserProfileInteractionIncrease(address _user, ValidInteraction memory _v) private {
-        UserProfile memory up = usersProfiles[_user];
+//         if (_expectedState != ps) {
+//             DETECTED_BAD_STATE_CHANGE = true;
+//         }
+//     }
 
-        up.callsLeft += _v.inputsNum;
-        up.hasCalls = true;
+//     function _updateUserProfileInteractionIncrease(address _user, ValidInteraction memory _v) private {
+//         UserProfile memory up = usersProfiles[_user];
 
-        usersProfiles[_user] = up;
-        usersProfiles[_user].validInteractions.push(_v);
-    }
+//         up.callsLeft += _v.inputsNum;
+//         up.hasCalls = true;
 
-    function _updateUserProfileInteractionDecrese(address _user, ValidInteraction memory _interactionConsumed)
-        private
-    {
-        UserProfile memory up = usersProfiles[_user];
+//         usersProfiles[_user] = up;
+//         usersProfiles[_user].validInteractions.push(_v);
+//     }
 
-        if (up.callsLeft > 0) {
-            up.callsLeft -= 1;
-        }
-        if (up.callsLeft == 0) {
-            up.hasCalls = false;
-        }
+//     function _updateUserProfileInteractionDecrese(address _user, ValidInteraction memory _interactionConsumed)
+//         private
+//     {
+//         UserProfile memory up = usersProfiles[_user];
 
-        usersProfiles[_user] = up;
-        ValidInteraction[] memory validInteractions = up.validInteractions;
-        for (uint256 i = 0; i < validInteractions.length; i++) {
-            if (_checkEqualInteraction(validInteractions[i], _interactionConsumed)) {
-                delete usersProfiles[_user].validInteractions[i];
-                usersProfiles[_user].invalidInteractionsIndexes.push(i);
-            }
-        }
-        usersProfiles[_user] = up;
-    }
+//         if (up.callsLeft > 0) {
+//             up.callsLeft -= 1;
+//         }
+//         if (up.callsLeft == 0) {
+//             up.hasCalls = false;
+//         }
 
-    function _checkEqualInteraction(ValidInteraction memory _v1, ValidInteraction memory _v2)
-        private
-        pure
-        returns (bool)
-    {
-        bool equal = true;
-        if (_v1.contractAddr != _v2.contractAddr) equal = false;
-        if (_v1.inputsNum != _v2.inputsNum) equal = false;
-        if (_v1.isSequence != _v2.isSequence) equal = false;
-        return equal;
-    }
+//         usersProfiles[_user] = up;
+//         ValidInteraction[] memory validInteractions = up.validInteractions;
+//         for (uint256 i = 0; i < validInteractions.length; i++) {
+//             if (_checkEqualInteraction(validInteractions[i], _interactionConsumed)) {
+//                 delete usersProfiles[_user].validInteractions[i];
+//                 usersProfiles[_user].invalidInteractionsIndexes.push(i);
+//             }
+//         }
+//         usersProfiles[_user] = up;
+//     }
 
-    /**
-     * INTERACTIONS
-     */
+//     function _checkEqualInteraction(ValidInteraction memory _v1, ValidInteraction memory _v2)
+//         private
+//         pure
+//         returns (bool)
+//     {
+//         bool equal = true;
+//         if (_v1.contractAddr != _v2.contractAddr) equal = false;
+//         if (_v1.inputsNum != _v2.inputsNum) equal = false;
+//         if (_v1.isSequence != _v2.isSequence) equal = false;
+//         return equal;
+//     }
 
-    function contractGivesPermission(uint256 _randomIndex, address _someone, uint256 _numOfInputs, bool _isSeq)
-        public
-    {
-        if (_someone == address(0)) _someone = address(69);
-        _numOfInputs = bound(_numOfInputs, 0, 10);
-        uint256 i = _randomIndex % contractsImplemetingICG.length;
-        UseCaseContractGlobal c = UseCaseContractGlobal(contractsImplemetingICG[i]);
-        address owner = ownersOfContracts[i];
+//     /**
+//      * INTERACTIONS
+//      */
 
-        bytes32[] memory inputsIds = new bytes32[](_numOfInputs);
-        for (i = 0; i < inputsIds.length; i++) {
-            inputsIds[i] = inputs[i].id;
-        }
+//     function contractGivesPermission(uint256 _randomIndex, address _someone, uint256 _numOfInputs, bool _isSeq)
+//         public
+//     {
+//         if (_someone == address(0)) _someone = address(69);
+//         _numOfInputs = bound(_numOfInputs, 0, 10);
+//         uint256 i = _randomIndex % contractsImplemetingICG.length;
+//         UseCaseContractGlobal c = UseCaseContractGlobal(contractsImplemetingICG[i]);
+//         address owner = ownersOfContracts[i];
 
-        vm.prank(owner);
-        c.giveInputPermission(_someone, inputsIds, myFuncSig, _isSeq);
+//         bytes32[] memory inputsIds = new bytes32[](_numOfInputs);
+//         for (i = 0; i < inputsIds.length; i++) {
+//             inputsIds[i] = inputs[i].id;
+//         }
 
-        IInputControlGlobal.PermissionState expected;
-        if (_isSeq) {
-            expected = IInputControlGlobal.PermissionState.IS_SEQUENCE;
-        } else {
-            expected = IInputControlGlobal.PermissionState.IS_UNORDERED;
-        }
-        _updateUserState(_someone, address(c), owner, myFuncSelec, expected);
-        usersWithProfiles.push(_someone);
-        ValidInteraction memory v =
-            ValidInteraction({contractAddr: address(c), inputsNum: _numOfInputs, isSequence: _isSeq});
-        _updateUserProfileInteractionIncrease(_someone, v);
-    }
+//         vm.prank(owner);
+//         c.giveInputPermission(_someone, inputsIds, myFuncSig, _isSeq);
 
-    // TODO
-    // function executingPermittedCall(uint256 _randomIndex) public {
-    //     if (usersWithProfiles.length == 0) return;
+//         IInputControlGlobal.PermissionState expected;
+//         if (_isSeq) {
+//             expected = IInputControlGlobal.PermissionState.IS_SEQUENCE;
+//         } else {
+//             expected = IInputControlGlobal.PermissionState.IS_UNORDERED;
+//         }
+//         _updateUserState(_someone, address(c), owner, myFuncSelec, expected);
+//         usersWithProfiles.push(_someone);
+//         ValidInteraction memory v =
+//             ValidInteraction({contractAddr: address(c), inputsNum: _numOfInputs, isSequence: _isSeq});
+//         _updateUserProfileInteractionIncrease(_someone, v);
+//     }
 
-    //     uint256 i = _randomIndex % usersWithProfiles.length;
-    //     address user = usersWithProfiles[i];
-    //     UserProfile memory up = usersProfiles[user];
+//     // TODO
+//     // function executingPermittedCall(uint256 _randomIndex) public {
+//     //     if (usersWithProfiles.length == 0) return;
 
-    //     // Iterate over users till finding one, if after all check none has, return
-    //     // while(!up.hasCalls) {
+//     //     uint256 i = _randomIndex % usersWithProfiles.length;
+//     //     address user = usersWithProfiles[i];
+//     //     UserProfile memory up = usersProfiles[user];
 
-    //     // }
-    //     // i = _randomIndex % up.validInteractions.length;
-    //     // Same here, iterate over validInteractions until finding one
-    //     // ValidInteraction memory vi = up.validInteractions[i];
-    // }
+//     //     // Iterate over users till finding one, if after all check none has, return
+//     //     // while(!up.hasCalls) {
 
-    // TODO
-    // Someone gives permissions in a not owned contract
-    // Execute random call, if invalid just expect revert
-}
+//     //     // }
+//     //     // i = _randomIndex % up.validInteractions.length;
+//     //     // Same here, iterate over validInteractions until finding one
+//     //     // ValidInteraction memory vi = up.validInteractions[i];
+//     // }
+
+//     // TODO
+//     // Someone gives permissions in a not owned contract
+//     // Execute random call, if invalid just expect revert
+// }
--- test/foundry/ICGInvariants.t.sol
+++ test/foundry/ICGInvariants.t.sol
@@ -1,59 +1,61 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-import {Test} from "../../lib/forge-std/src/Test.sol";
-import "../../lib/forge-std/src/console.sol";
+// TODO: ON DEVELOPMENT
 
-// Composite (Global) version
-import {InputControlGlobal} from "contracts/decentralized/globalVersion/InputControlGlobal.sol";
-import {IInputControlGlobal} from "contracts/decentralized/globalVersion/IInputControlGlobal.sol";
-import {UseCaseContractGlobal} from "contracts/decentralized/globalVersion/UseCaseContractGlobal.sol";
+// import {Test} from "../../lib/forge-std/src/Test.sol";
+// import "../../lib/forge-std/src/console.sol";
 
-import {HandlerICG} from "./ICGHandler.t.sol";
+// // Composite (Global) version
+// import {InputControlGlobal} from "contracts/decentralized/globalVersion/InputControlGlobal.sol";
+// import {IInputControlGlobal} from "contracts/decentralized/globalVersion/IInputControlGlobal.sol";
+// import {UseCaseContractGlobal} from "contracts/decentralized/globalVersion/UseCaseContractGlobal.sol";
 
-contract InvariantsICG is Test {
-    uint256 public constant NUM_OF_PARTICIPANT_CONTRACTS = 10;
-    address[] public owners;
-    address[] public contracts;
-    address public neverCalled;
-    address public ownerNeverCalled;
+// import {HandlerICG} from "./ICGHandler.t.sol";
 
-    IInputControlGlobal inputCGlobal;
-    HandlerICG public handler;
+// contract InvariantsICG is Test {
+//     uint256 public constant NUM_OF_PARTICIPANT_CONTRACTS = 10;
+//     address[] public owners;
+//     address[] public contracts;
+//     address public neverCalled;
+//     address public ownerNeverCalled;
 
-    function setUp() public {
-        inputCGlobal = IInputControlGlobal(new InputControlGlobal());
-        _deployXUseCaseContracts(NUM_OF_PARTICIPANT_CONTRACTS);
-        HandlerICG h = new HandlerICG(contracts, owners, address(inputCGlobal));
-        targetContract(address(h));
-        excludeSender(ownerNeverCalled);
-    }
+//     IInputControlGlobal inputCGlobal;
+//     HandlerICG public handler;
 
-    // Setting functions
-    function _deployXUseCaseContracts(uint256 x) private {
-        for (uint256 i = 0; i < x; i++) {
-            address owner;
-            owner = address(uint160(i));
-            vm.prank(owner);
-            UseCaseContractGlobal c = new UseCaseContractGlobal(address(inputCGlobal));
-            contracts.push(address(c));
-            owners.push(owner);
-        }
-    }
+//     function setUp() public {
+//         inputCGlobal = IInputControlGlobal(new InputControlGlobal());
+//         _deployXUseCaseContracts(NUM_OF_PARTICIPANT_CONTRACTS);
+//         HandlerICG h = new HandlerICG(contracts, owners, address(inputCGlobal));
+//         targetContract(address(h));
+//         excludeSender(ownerNeverCalled);
+//     }
 
-    // Invariants
-    function invariant_StatesChangedCorectly() public {
-        bool detected = handler.DETECTED_BAD_STATE_CHANGE();
-        assertFalse(detected);
-    }
+//     // Setting functions
+//     function _deployXUseCaseContracts(uint256 x) private {
+//         for (uint256 i = 0; i < x; i++) {
+//             address owner;
+//             owner = address(uint160(i));
+//             vm.prank(owner);
+//             UseCaseContractGlobal c = new UseCaseContractGlobal(address(inputCGlobal));
+//             contracts.push(address(c));
+//             owners.push(owner);
+//         }
+//     }
 
-    function invariant_ContractsAccessedOnlyWithPermission() public {
-        bool detected = handler.DETECTED_USED_CONTRACT_WITHOUT_PERMISSION();
-        assertFalse(detected);
-    }
+//     // Invariants
+//     function invariant_StatesChangedCorectly() public {
+//         bool detected = handler.DETECTED_BAD_STATE_CHANGE();
+//         assertFalse(detected);
+//     }
 
-    function invariant_NonExistingStatesOccupyNoStorage() public {
-        bool detected = handler.DETECTED_BAD_STORAGE_CLEAN_UP();
-        assertFalse(detected);
-    }
-}
+//     function invariant_ContractsAccessedOnlyWithPermission() public {
+//         bool detected = handler.DETECTED_USED_CONTRACT_WITHOUT_PERMISSION();
+//         assertFalse(detected);
+//     }
+
+//     function invariant_NonExistingStatesOccupyNoStorage() public {
+//         bool detected = handler.DETECTED_BAD_STORAGE_CLEAN_UP();
+//         assertFalse(detected);
+//     }
+// }
